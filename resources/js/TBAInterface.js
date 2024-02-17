0f71cae17c8b5bd9033623e5c0b2454312b2e697
YP10 <yoshi.patel10@gmail.com>
2024-02-16 22:33:52 -0500

    please work

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "mMvBedxKmDJOm8bCbCh5BU9EmhWFfM6maW5swe7BpFJttV4i92NArIuIWdyAO49h";
 /**
  * Get list of teams in event
  *
